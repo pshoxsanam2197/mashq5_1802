17870a18fdfa77d76d837574e429e633c74ae7dc
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2026-02-18 17:14:05 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+# 1-masala
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+intersection = A & B
+
+print(intersection)
+
+# 2-masala
+A = {10, 20, 30}
+B = {30, 40, 50}
+
+union = A | B
+print(union)
+
+3-masala
+numbers = {5, 10, 15, 20, 25}
+
+count = len(numbers)
+
+print(count)
+
+# 4-masala
+numbers = {100, 200, 300, 400}
+
+numbers.remove(300)
+print(numbers)
+
+# 5-masala
+numbers = [1, 2, 2, 3, 4, 4, 5]
+unique_numbers = set(numbers)
+print(unique_numbers)
